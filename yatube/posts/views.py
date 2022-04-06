2e154c00f5c6232ae1cd154cdc95ca59a6306b0a
Дима Киреев <kireev.d2012@yandex.ru>
2022-04-07 01:42:20 +0300

    One

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
+
 from django.core.paginator import Paginator
+
 from django.shortcuts import get_object_or_404, redirect, render
+
 from .forms import CommentForm, PostForm
 
 from .forms import PostForm
@@ -65,8 +68,9 @@ def profile(request, username):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     following = (request.user.is_authenticated
-                    and author.following.filter(
+                and author.following.filter(
                     user=request.user,
+                    author=author
                 ).exists())
     context = {
         'author': author,
