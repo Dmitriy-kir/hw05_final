9d5db2dc7e324bfa75daa6f0b2545f8ef45c66a8
Дима Киреев <kireev.d2012@yandex.ru>
2022-04-07 01:36:56 +0300

    One

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -65,7 +65,7 @@ def profile(request, username):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     following = (request.user.is_authenticated
-                and author.following.filter(
+                    and author.following.filter(
                     user=request.user,
                 ).exists())
     context = {
